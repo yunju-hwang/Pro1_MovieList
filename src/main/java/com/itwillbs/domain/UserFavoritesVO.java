c632afe1c1082663afdf7249c485621e83efb509
nkb0717 <nkb0717@gmail.com>
2025-11-12 12:51:34 +0900

    2025-11-12(1)
    123

--- src/main/java/com/itwillbs/domain/UserFavoritesVO.java
+++ src/main/java/com/itwillbs/domain/UserFavoritesVO.java
@@ -13,4 +13,11 @@ public class UserFavoritesVO {
 	private int id;
     private String userId;
     private int tmdbId;
+    
+    private String movie_title;   
+    private String poster_path;
+    private String release_date;
+    private String overview;      
+    private int runtime;          
+    private double popularity;    
 }
